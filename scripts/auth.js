962e700f54e6c54a7aec380bbedda2d6c2305ffa
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-11-09 12:14:28 -0300

    LogOut and Login

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,24 +1,42 @@
-//const keys = require('../configuration/keys');
-
 // singup
 const signupForm = document.querySelector('#signup-form');
-    signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    // Get user Info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
+  // Get user Info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
 
-    //console.log(email, password)
+  //console.log(email, password)
 
-    //sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
-        console.log(cred.user);
+  //sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+    //console.log(cred.user);
 
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
 
-        signupForm.reset();
-    })
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {});
+});
 
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // Get user Info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((response) => {
+    console.log(response);
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
 });
