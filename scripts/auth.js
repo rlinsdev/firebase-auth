6a27fbb4b33183af24782de8db670bb2744dedbf
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-11-09 09:21:35 -0300

    Signup

---
+++ scripts/auth.js
@@ -0,0 +1,24 @@
+//const keys = require('../configuration/keys');
+
+// singup
+const signupForm = document.querySelector('#signup-form');
+    signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get user Info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    //console.log(email, password)
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+        console.log(cred.user);
+
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+
+        signupForm.reset();
+    })
+
+});
